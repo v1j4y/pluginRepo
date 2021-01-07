3dc65b5169a2d480d1ddf8952a4b770e63920d85
v1j4y <vijay.gopal.c@gmail.com>
2021-01-07 21:53:41 +0100

    Added utility functions.

--- cfgCI/cfgCI.irp.f
+++ cfgCI/cfgCI.irp.f
@@ -3,5 +3,14 @@ program cfgCI
   BEGIN_DOC
 ! TODO : Put the documentation of the program here
   END_DOC
+  character*32 cc
+  print *, cfg_seniority_index
+  call debug_det(psi_det(1,1,1),N_int)
+  call debug_det(psi_det(1,1,2),N_int)
+  call debug_spindet(psi_det(1,1,1),N_int)
+  call debug_spindet(psi_det(1,1,2),N_int)
+  print *,N_int
+  cc = "Hello"
+  call printCFGlist(cfg_seniority_index,15)
   print *, 'Hello world'
 end
