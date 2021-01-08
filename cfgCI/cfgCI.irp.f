63e732d2ee79c00246e3f3c6ccc3a01b11162b1a
v1j4y <vijay.gopal.c@gmail.com>
2021-01-08 15:55:00 +0100

    working on calling c functions with fortran lists.

--- cfgCI/cfgCI.irp.f
+++ cfgCI/cfgCI.irp.f
@@ -4,13 +4,16 @@ program cfgCI
 ! TODO : Put the documentation of the program here
   END_DOC
   character*32 cc
-  print *, cfg_seniority_index
+  print *, N_int
+  print *, psi_configuration(1,1,:200)
+  print *, psi_configuration(1,2,:200)
   call debug_det(psi_det(1,1,1),N_int)
   call debug_det(psi_det(1,1,2),N_int)
   call debug_spindet(psi_det(1,1,1),N_int)
   call debug_spindet(psi_det(1,1,2),N_int)
   print *,N_int
   cc = "Hello"
-  call printCFGlist(cfg_seniority_index,15)
+  print *,psi_det_size
+  call printCFGlist(cfg_seniority_index,N_int,psi_det_size)
   print *, 'Hello world'
 end
